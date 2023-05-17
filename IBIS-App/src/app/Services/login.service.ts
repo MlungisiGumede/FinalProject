fec798958ece57559c503422988c985ed3018011
AlbertusBB <u20487755@tuks.co.za>
2023-05-17 13:59:47 +0200

    first service integration and api call functionality
    getting the api calls going and making sure the data is passed through

---
+++ IBIS-App/src/app/Services/login.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
+import { retry, catchError } from 'rxjs/operators';
+import { User } from '../Models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  apiUrl = 'https://localhost:7226/api/User';
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  httpOptions = {
+    headers: new HttpHeaders({
+     'Content-Type': 'application/json'
+     })
+      }
+
+    public getUserList(): Observable<User[]> {
+      return this.httpClient
+        .get<User[]>(this.apiUrl)
+    }
+
+
+  }
+
+        
