c2c035fd9dbb3b92bfc2e60b95e888b8bfa0f035
AlbertusBB <u20487755@tuks.co.za>
2023-07-11 12:07:26 +0200

    Update Inventory Service and component changes
    Inventory

---
+++ IBIS-App/src/app/Models/Inventory.ts
@@ -0,0 +1,7 @@
+export class Inventory{
+
+    InventoryId?: string;
+    Name?: string;
+    SKU?: string;
+    Quantity?: number;
+  }
---
+++ IBIS-App/src/app/Services/inventory.service.ts
@@ -0,0 +1,86 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, retry, throwError } from 'rxjs';
+import { Inventory } from '../Models/Inventory';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InventoryService {
+
+
+  apiUrl = 'https://localhost:7226/api/Products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+     'Content-Type': 'application/json'
+     })
+      }
+
+
+
+      private handleError(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+          console.error('An error occurred:', error.error.message);
+        } else {
+          console.error(
+            `Backend returned code ${error.status}, ` +
+            `body was: ${error.error}`);
+        }
+        return throwError('Something bad happened; please try again later.'); 
+      }
+      
+      
+      
+      
+       createInventory(inv: Inventory){
+        return this.httpClient.post(this.apiUrl, inv, this.httpOptions)
+      }
+  
+    
+    
+       getInventoryList(): Observable<Inventory> {
+         return this.httpClient
+         .get<Inventory>(this.apiUrl + '/getAll')
+           .pipe(
+             retry(2),
+             catchError(this.handleError)
+           )
+       }
+    
+       getinv(id: any): Observable<Inventory> {
+         return this.httpClient
+           .get<Inventory>(this.apiUrl + "/" +id)
+           .pipe(
+             retry(2),
+             catchError(this.handleError)
+           )
+       }
+      
+       updateInventory(id: any, data: any): Observable<Inventory> {
+         return this.httpClient
+           .put<Inventory>(this.apiUrl + '/' + id, data, this.httpOptions)
+           .pipe(
+             retry(2),
+             catchError(this.handleError)
+           )
+       }
+      
+       delete(id:number){
+        return this.httpClient.delete<Inventory>(`${this.apiUrl}/${id}` , this.httpOptions);
+     }
+
+
+
+    
+       deleteInventory(id: string): Observable<any> {
+        return this.httpClient.delete(`${this.apiUrl}/${id}` , this.httpOptions)
+          .pipe(
+            catchError(this.handleError)
+          );
+      }
+     }
+
+
